9c1087c2b9b9c7a3e51e5600378af90f34c7d216
abubakar8383 <abumira83@gmail.com>
2020-12-07 14:57:56 +0530

    test

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
 var ball;
+var database
+var databaseposition
 
 function setup(){
     createCanvas(500,500);
+    database=firebase.database();
     ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball.shapeColor = "red"; 
+    var getinfo = database.ref("Ball/position")
+    getinfo.on("value",readop,showerr)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("Ball/position").set({
+        x:ball.x+x,
+        y:ball.y+y
+    })
+}
+function readop(data){
+databaseposition=data.val();
+ball.x=databaseposition.x
+ball.y=databaseposition.y
+}
+function showerr(){
+    console.log("error")
 }
